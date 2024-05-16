a85156d971be5059fec63ec41ff6f36d8aaa7ded
ericel7erek <ericalaa@ymail.com>
2024-05-16 18:27:37 +0200

    haaa

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const saltRounds = 10;
 const cors = require('cors');
 const app = express()
 const knex = require('knex')
-
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; 
 const db = knex({
   client: 'pg',
   connection: {
