abcf1eaa4d92ac005f1f5b4f27b7016a8b0128e4
ericel7erek <ericalaa@ymail.com>
2024-05-16 17:45:24 +0200

    Hopefully

--- server.js
+++ server.js
@@ -8,14 +8,13 @@ const knex = require('knex')
 const db = knex({
   client: 'pg',
   connection: {
-    host: 'https://smart-brain-api-g8ui.onrender.com',
-    hostname: 'dpg-cp31hb63e1ms73f827hg-a',
-    port: 5432,
-    user: 'smart_brain_3feo_user',
-    password: 'DovUT7RUrMRFfA396ADYyE1OJgTMevz2',
-    database: 'smart_brain_3feo',
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
   },
 });
+
 const  port= process.env.PORT || 5432
 app.use(express.json())
 app.use(cors())
