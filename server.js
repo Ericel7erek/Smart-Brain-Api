2eab66ec61b34342d69906713cb4f1ce5cfd69fa
ericel7erek <ericalaa@ymail.com>
2024-05-16 17:07:53 +0200

    hee7

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ const knex = require('knex')
 const db = knex({
   client: 'pg',
   connection: {
-    host: process.env.DATABASE_URL
+    host: 'postgres://smart_brain_3feo_user:DovUT7RUrMRFfA396ADYyE1OJgTMevz2@dpg-cp31hb63e1ms73f827hg-a/smart_brain_3feo',
+    port: 5432,
+    user: 'smart_brain_3feo_user',
+    password: 'DovUT7RUrMRFfA396ADYyE1OJgTMevz2',
+    database: 'smart_brain_3feo',
   },
 });
 const  port= process.env.PORT || 5432
